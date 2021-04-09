9381804a07b5f1716a524d46e77afda9fbd504cf
Darelllf <d.legoferdanu@gmail.com>
2021-04-09 09:29:28 +0700

    Initial Commit

---
+++ MyGunung/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyGunung
+//
+//  Created by Darell Legoferdanu on 09/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
